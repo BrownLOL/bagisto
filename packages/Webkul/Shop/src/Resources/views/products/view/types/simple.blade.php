e6d6120045597cc3336cf91a5424a4bfff2f2a8f
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-07 17:23:27 +0800

    debug: disabled customizable-options to test Vue error

--- packages/Webkul/Shop/src/Resources/views/products/view/types/simple.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/types/simple.blade.php
@@ -1,4 +1,4 @@
-@include('shop::products.view.customizable-options')
+{{-- @include('shop::products.view.customizable-options') --}}
 
 {{-- Product Customization Button --}}
 @include('shop::products.view.customization-button')
