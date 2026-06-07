17b43531923b0684c4bd6916b74327fa675b3ae4
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-07 17:26:36 +0800

    debug: added simple JS test

--- packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
@@ -1,3 +1,8 @@
 <div id="test-customization">
     <p>Customization Test - Product ID: <?php echo $product->id; ?></p>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log('Test script loaded for product: ' + document.querySelector('#test-customization p').textContent);
+        });
+    </script>
 </div>
