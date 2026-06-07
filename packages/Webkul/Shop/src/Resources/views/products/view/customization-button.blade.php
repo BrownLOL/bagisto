48895cfcab1d90acff285b9b28c45687e25b7a9d
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-07 18:06:37 +0800

    debug: no script tag version

--- packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
@@ -2,14 +2,7 @@
     <button
         id="customize-btn"
         class="px-6 py-3 bg-primary text-white rounded-lg hover:bg-primary-darker transition-colors"
-        onclick="window.location.href = '/customization/designer/' + 1"
     >
         立即定制
     </button>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            console.log('Customization initialized');
-        });
-    </script>
 </div>
