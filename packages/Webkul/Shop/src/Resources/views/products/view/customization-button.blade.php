6aa519ffa940656c06c3f4e15b07346f2dd7c771
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-07 19:38:32 +0800

    fix: correct API URL path for print-areas

--- packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/customization-button.blade.php
@@ -66,7 +66,7 @@ alert('脚本开始执行！');
     function loadPrintAreas(productId) {
         dialogContent.innerHTML = '<p style="text-align: center; padding: 40px;">加载中...</p>';
         
-        fetch('/api/product/' + productId + '/print-areas')
+        fetch('/print-areas/' + productId)
             .then(function(response) { return response.json(); })
             .then(function(data) {
                 console.log('Print areas data:', data);
