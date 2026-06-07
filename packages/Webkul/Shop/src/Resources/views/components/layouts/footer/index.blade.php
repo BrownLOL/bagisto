ea1ef1912825f7f051cbea71c3b0bad86bda56ed
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-07 16:59:21 +0800

    fix: footer_links padding using inline style
    Coze-Commit-Type: user
    Coze-User-ID: 4337991133177372
    Coze-Conversation-ID: 4912498

--- packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
@@ -23,7 +23,7 @@
 
 <footer class="mt-9 bg-lightOrange max-sm:mt-10">
     <div class="container mx-auto max-w-[1200px]">
-    <div class="flex justify-between gap-x-6 gap-y-8 py-[60px] max-1060:flex-col-reverse max-md:gap-5 max-sm:py-5">
+    <div class="flex justify-between gap-x-6 gap-y-8 max-1060:flex-col-reverse max-md:gap-5 max-sm:py-5" style="padding-top: 60px; padding-bottom: 60px;">
         <!-- For Desktop View -->
         <div
             class="flex flex-wrap items-start gap-24 max-1180:gap-6 max-1060:hidden"
