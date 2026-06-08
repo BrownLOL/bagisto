b8e236d7ddbee13fefa23378d31af6775d42d273
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-08 23:13:39 +0800

    fix: Save按钮去除text-white加上border
    Coze-Commit-Type: user
    Coze-User-ID: 4337991133177372
    Coze-Conversation-ID: 4912498

--- packages/Webkul/Shop/src/Resources/views/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view.blade.php
@@ -234,7 +234,7 @@
         
         <div class="flex justify-end gap-2 mt-4 pt-4 border-t">
             <button id="cancel-btn" onclick="closeDialog()" class="px-4 py-2 border rounded hover:bg-gray-100">Cancel</button>
-            <button class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Save</button>
+            <button class="px-4 py-2 bg-blue-600 border rounded hover:bg-blue-700">Save</button>
         </div>
     </div>
 </div>
