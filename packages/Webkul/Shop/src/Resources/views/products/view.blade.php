d98be12c1a4b6f2598bf63a54511632480683494
user1969259093 <4337991133177372-user1969259093@noreply.coze.cn>
2026-06-13 20:55:07 +0800

    Restored to '6f2caace73bde42e62dce0b275ae4c3bc4cc1b22'
    Coze-Restored-To: '6f2caace73bde42e62dce0b275ae4c3bc4cc1b22'
    Coze-Commit-Type: restore
    Coze-User-ID: 4337991133177372

--- packages/Webkul/Shop/src/Resources/views/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view.blade.php
@@ -255,8 +255,6 @@
     </div>
 </div>
 
-@include('shop::products.view.customization-button')
-
     <!-- Information Section -->
     <div class="1180:mt-20">
         <div class="max-1180:hidden">
